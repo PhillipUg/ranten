86b8ded9e04697a7984a522d2de342a58c4f8721
phillipug <phillipmusiime@gmail.com>
2020-06-19 15:36:10 +0300

    added homepage controller, view & route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+	root 'homepage#home'
+  resources :users 
+
+  resources :rants
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
