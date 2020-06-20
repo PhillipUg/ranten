16dbc3ec0956ea5b80a3393e3738651347f8ad94
phillipug <phillipmusiime@gmail.com>
2020-06-21 00:56:09 +0300

    added unfollow route

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
  
   resources :users do
  		post 'follow', to: 'users#follow', as: 'follow'
+ 		delete 'unfollow', to: 'users#unfollow', as: 'unfollow'
   end
 
   resources :rants
