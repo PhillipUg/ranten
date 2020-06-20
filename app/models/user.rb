601d3b3c939b3c7a33a00320a119670d9adf76f4
phillipug <phillipmusiime@gmail.com>
2020-06-21 01:02:49 +0300

    added validations & instance methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
 	has_many :rants, foreign_key: :author_id
+	
+	has_many :followings, foreign_key: :follower_id
+	has_many :followers, class_name: :Following, foreign_key: :followed_id
 
-	has_many :followings, foreign_key: :followed_id
-	has_many :followers, through: :followings, source: :follower
+	validates :username, presence: true, uniqueness: true, length: {minimum: 3, maximum: 50}
+	validates :full_name, presence: true
+
+	def followed_users
+		followings.map {|following| following.followed }.compact
+	end
+
+	def users_following
+		followers.map {|following| following.follower }.compact
+	end
 end
