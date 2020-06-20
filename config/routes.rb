72565dc264a5195ef9c9e10876d949887cdccaa9
phillipug <phillipmusiime@gmail.com>
2020-06-20 22:20:25 +0300

    added follow route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
-	root 'homepage#home'
+	root 'rants#index'
+
+	get 'home', to: 'homepage#home', as: 'home'
 
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
  
-  resources :users 
+  resources :users do
+ 		post 'follow', to: 'users#follow', as: 'follow'
+  end
 
   resources :rants
 end
