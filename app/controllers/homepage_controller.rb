d4d11f68042c704c9327846686adeef4fb94e506
phillipug <phillipmusiime@gmail.com>
2020-06-21 21:50:32 +0300

    limited authorization callback to new & create actions

--- app/controllers/homepage_controller.rb
+++
@@ -1,5 +0,0 @@
-class HomepageController < ApplicationController
-
-	def home
-	end
-end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authorize, except: [:new, :create]
 
   # GET /users
   def index
