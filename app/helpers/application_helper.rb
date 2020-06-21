bdc601d42666aaa8f2b7af02ac0d946ba637433d
phillipug <phillipmusiime@gmail.com>
2020-06-21 21:51:09 +0300

    Added helper for default photo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+	def default_avatar(user)
+		if user.photo.attached?
+			image_tag(user.photo, width: 64, height: 64, class: "avatar")
+		else
+			image_tag("https://bulma.io/images/placeholders/64x64.png", class: "avatar")
+		end
+	end
+
+	def default_cover(user)
+		if user.cover_image.attached?
+			image_tag(user.cover_image, width: 720, height: 240)
+		else
+			image_tag("https://bulma.io/images/placeholders/720x240.png")
+		end
+	end
 end
