c810dcb90e9f0ef8071512abc7e9373ded7c9f22
phillipug <phillipmusiime@gmail.com>
2020-06-25 22:46:50 +0300

    added a couple of helper methods for views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,5 +16,35 @@ module ApplicationHelper
       image_tag('https://picsum.photos/id/0/720/240', class: 'cover-photo')
     end
   end
-  
+
+  def like_or_dislike_btn(rant)
+    like = Like.find_by(rant: rant, user: current_user)
+    if like
+      link_to('<i class="fa fa-heart" aria-hidden="true"></i>'.html_safe, rant_like_path(id: like.id, rant_id: rant.id), method: :delete, class: 'dislike')
+    else
+      link_to('<i class="far fa-heart" aria-hidden="true"></i>'.html_safe, rant_likes_path(rant_id: rant.id), method: :post, class: 'like')
+    end
+  end
+
+  def follow_plus(user)
+    if user != current_user
+      if user.users_following.include?(current_user)
+        link_to('<div class="icon"><i class="fas fa-times"></i></div>'.html_safe, user_unfollow_path(user.id), method: "DELETE", class: "follow-ex")
+      else
+        link_to('<div class="icon"><i class="fas fa-plus"></i></div>'.html_safe, user_follow_path(user.id), method: "POST", class: "follow-plus")
+      end
+    end   
+  end
+
+  def list_icon(user)
+    if user != current_user
+      '<div class="icon"><i class="fas fa-list-ul"></i></div>'.html_safe
+    end    
+  end
+
+  def follow_plus2(user)
+    if !current_user.followed_users.include?(user) && current_user != user
+      link_to('<div class="icon"><i class="fas fa-plus"></i></div>'.html_safe, user_follow_path(user.id), method: "POST", class: "follow-er")
+    end
+  end
 end
