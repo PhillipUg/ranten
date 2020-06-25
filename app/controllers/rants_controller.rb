f7ef873cf4e04932ec8c67c0988407fd6202e3f5
phillipug <phillipmusiime@gmail.com>
2020-06-25 22:45:52 +0300

    refactored controllers, removed redundant code

--- app/controllers/rants_controller.rb
+++ app/controllers/rants_controller.rb
@@ -1,25 +1,16 @@
 class RantsController < ApplicationController
-  before_action :set_rant, only: %i[show edit update destroy]
+  before_action :set_rant, only: %i[destroy]
   before_action :authorize
 
-  # GET /rants
   def index
+    @users = User.includes([:photo_attachment]).all.order('created_at DESC')
     @rants = Rant.all.order('created_at DESC').includes(:author)
-    @users = User.with_attached_photo.all.order('created_at DESC')
   end
 
-  # GET /rants/1
-  def show; end
-
-  # GET /rants/new
   def new
     @rant = current_user.rants.new
   end
 
-  # GET /rants/1/edit
-  def edit; end
-
-  # POST /rants
   def create
     @rant = current_user.rants.new(rant_params)
 
@@ -31,16 +22,6 @@ class RantsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /rants/1
-  def update
-    if @rant.update(rant_params)
-      redirect_to rants_path, notice: 'Rant was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /rants/1
   def destroy
     @rant.destroy
     redirect_to rants_url, alert: 'Rant deleted!'
@@ -48,12 +29,10 @@ class RantsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_rant
     @rant = Rant.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def rant_params
     params.permit(:content)
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,15 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[show edit update destroy]
+  before_action :set_user, only: %i[show]
   before_action :authorize, except: %i[new create]
 
-  # GET /users
-  def index
-    @users = User.with_attached_photo.all.order('created_at DESC')
-  end
-
-  # GET /users/1
   def show
     @followed_users = @user.followed_users
     @users_following = @user.users_following
   end
 
-  # GET /users/new
   def new
     @user = User.new
   end
-
-  # GET /users/1/edit
-  def edit; end
 
   def follow
     @following = Following.new(follower_id: current_user.id, followed_id: params[:user_id])
@@ -33,7 +23,6 @@ class UsersController < ApplicationController
     redirect_to user_path(params[:user_id]), alert: 'Unfollowed User'
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
     if @user.save
@@ -44,29 +33,12 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  def update
-    if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /users/1
-  def destroy
-    @user.destroy
-    redirect_to users_url, notice: 'User was successfully destroyed.'
-  end
-
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a trusted parameter "white list" through.
   def user_params
     params.require(:user).permit(:username, :full_name, :photo, :cover_image)
   end
